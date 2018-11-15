ef5fca1d25bfe6af9a2e429f75b225da30f95210
adrntr <adriantrillo10@gmail.com>
2018-11-15 20:57:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ingeniera/trakeoentregas/MapsActivity.java
@@ -0,0 +1,179 @@
+package com.example.ingeniera.trakeoentregas;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final int LOCATION_REQUEST = 500;
+    ArrayList<LatLng> listPoints;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        listPoints=new ArrayList<>();
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST);
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        googleMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                //reset marker when already 2
+                if(listPoints.size()==2){
+                    listPoints.clear();
+                    mMap.clear();
+                }
+                listPoints.add(latLng);
+                //create maker
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                if(listPoints.size()==1){
+                    //add first marker to the map
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                }else{
+                    //add second marker to the map
+                    markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                }
+                mMap.addMarker(markerOptions);
+                //TODO: request get direction code bellow
+                if(listPoints.size()==2){
+                    //create the URL to get request from first marker to second marker
+                    String url = getRequestedUrl(listPoints.get(0),listPoints.get(1));
+                }
+            }
+        });
+    }
+
+    private String getRequestedUrl(LatLng origin, LatLng dest) {
+        //value of origin =
+        String str_org = "origin="+origin.latitude+","+origin.longitude;
+        //value of detination
+        String str_dest = "destination="+dest.latitude+","+dest.longitude;
+        //set value enable the sensor
+        String sensor="sensor=false";
+        //mode for find direction
+        String mode= "mode=driving";
+        //build the full param
+        String param = str_org+"&"+str_dest+"&"+sensor+"&"+mode;
+        String output = "json";
+        //create url to request
+        String url = "https://maps.googleapis.com/maps/direction/"+output+"?"+param;
+        return url;
+
+    }
+
+    //a partir de una url obtiene los datos de como llegar al destino
+    private String requestDirection(String reqUrl) throws IOException {
+        String responseString="";
+        InputStream inputStream=null;
+        HttpURLConnection httpURLConnection = null;
+        try{
+            URL url=new URL(reqUrl);
+            httpURLConnection=(HttpURLConnection)url.openConnection();
+            httpURLConnection.connect();
+
+            //Get the response
+            inputStream=httpURLConnection.getInputStream();
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            StringBuffer stringBuffer = new StringBuffer();
+            String line="";
+            while ((line=bufferedReader.readLine())!=null){
+                stringBuffer.append(line);
+            }
+
+            responseString = stringBuffer.toString();
+            bufferedReader.close();
+            inputStreamReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if (inputStream!=null){
+                inputStream.close();
+            }
+            httpURLConnection.disconnect();
+        }
+
+        return responseString;
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case LOCATION_REQUEST:
+                if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    mMap.setMyLocationEnabled(true);
+                }
+                break;
+        }
+    }
+
+    public class TaskRequestDirections extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String responseString="";
+            try {
+                responseString=requestDirection(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+}
